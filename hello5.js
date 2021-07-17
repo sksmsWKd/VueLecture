94a11a24e456cb92beea5761f936b964d394900a
Song Jae Hyeon <0nocsace@gmail.com>
2021-07-17 23:44:23 +0900

    07/17 Vue for in statement

---
+++ hello5.js
@@ -0,0 +1,15 @@
+let app = new Vue({
+        el: '#app',
+        data: {
+                todos: [
+                        { text: 'Javascript 배우기' },
+                        { text: 'vue 배우기' },
+                        { text : '멋진 것 만들기'}
+                ],
+                todos2: [
+                        { text: 'zz' },
+                        { text: 'xx' },
+                        { text : 'cc'}
+                ]
+        }
+})
