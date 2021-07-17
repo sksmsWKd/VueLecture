4a9fc63f07d621783d38f248cb6ce8df8df393a4
Song Jae Hyeon <0nocsace@gmail.com>
2021-07-17 23:45:25 +0900

    07/17 Vue for in value,key

---
+++ hello8.js
@@ -0,0 +1,10 @@
+let app = new Vue({
+        el: '#app',
+        data: {
+                user: {
+                        userid: 'user123',
+                        name: 'Jane Vue',
+                        email: 'user123@email.com'
+                }
+        }
+})
